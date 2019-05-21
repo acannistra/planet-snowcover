d242b9605b45ec5094c608ca0a52d9baabcf5567
Tony Cannistra (EC2) <tony.cannistra@gmail.com>
2019-05-21 18:44:28 +0000

    updates

--- model/predict.py
+++ model/predict.py
@@ -11,10 +11,12 @@ import pkgutil
 import boto3
 import s3fs
 import argparse
+from tqdm import tqdm
 
 from re import match
 
 import sklearn
+from PIL import Image
 
 from numpy.random import randint
 import numpy as np
@@ -26,6 +28,8 @@ from robosat_pink.tools.train import get_dataset_loaders
 from robosat_pink.config import load_config
 from robosat_pink.logs import Logs
 from robosat_pink.metrics import Metrics
+from robosat_pink.colors import make_palette
+
 
 import torch
 import torch.backends.cudnn
@@ -39,34 +43,74 @@ def main():
     ap.add_argument('model', help='path to model checkpoint')
     ap.add_argument('config', help='path to model config')
     ap.add_argument('tiles', help='path to XYZ tile folder')
+    ap.add_argument('outputdir', help='name for tile output')
     ap.add_argument('--aws_profile', help='AWS Profile Name', default = 'default')
+
 
     args = ap.parse_args()
 
     config = load_config(args.config)
-    tiles = S3SlippyMapTiles(args.tiles, mode = 'multimode', aws_profile = args.aws_profile)
-    net = model(config)
+    tiles = S3SlippyMapTiles(args.tiles, mode = 'multibands', aws_profile = args.aws_profile)
+    net = model(config, args.model)
 
 
-    loader = DataLoader(train_tileset,
+    loader = DataLoader(tiles,
                         batch_size = config['model']['batch_size'],
                         shuffle = True,
                         num_workers = 1)
+    
+    palette = make_palette(config["classes"][0]["color"])
 
-    predict(net, loader)
+    fs = s3fs.S3FileSystem(session = boto3.Session(profile_name = 'esip'))
+    
+    outputdir = args.outputdir[5:] + '/' + os.path.basename(args.tiles)
+    print("Saving predictions to {}.".format(outputdir))
+    
+    predict(net, loader, outputdir , palette, fs)
 
 
 
 
-def predict(net, loader):
+def predict(net, loader, outputdir, palette, fs):
     net.eval()
 
-    for _tile, image in tqdm(loader, desc="Predict", unit="batch", ascii=True):
-        print(_tile)
-        # with torch.no_grad():
-        #     raw = model(images)
+    for _tiles, images in tqdm(loader, desc="Predict", unit="batch", ascii=True):
 
-def model(config):
+        _tiles = list(zip(_tiles[2].cpu().numpy(),
+                          _tiles[0].cpu().numpy(),
+                          _tiles[1].cpu().numpy())) # idk why this happens 
+        
+        print(_tiles)
+        with torch.no_grad():
+            raw = net(images)
+        
+        print(len(_tiles), len(raw))
+        for tile, raw_prob in zip(_tiles, raw):
+            print(tile)
+            z, x, y = tile
+
+            prob = raw_prob > 0 
+
+            image = prob.cpu().numpy().astype(np.uint8).squeeze()
+
+            out = Image.fromarray(image, mode="P")
+            out.putpalette(palette)
+
+            path = os.path.join(outputdir, str(z), str(x), str(y) + ".png")
+            print('writing {}'.format(path))
+            with fs.open(path, 'wb') as f:
+                out.save(f, format='png', optimize=True)
+                
+def model(config, trainedModel):
+    
+    S3_CHECKPOINT = False
+    if trainedModel.startswith("s3://"):
+        S3_CHECKPOINT = True
+        # load from s3 
+        trainedModel = trainedModel[5:]
+        sess = boto3.Session(profile_name='esip')
+        fs = s3fs.S3FileSystem(session=sess)
+    
 
     if torch.cuda.is_available():
         device = torch.device("cuda")
@@ -114,7 +158,11 @@ def model(config):
     loss_module = import_module("robosat_pink.losses.{}".format(config["model"]["loss"]))
     criterion = getattr(loss_module, "{}".format(config["model"]["loss"].title()))().to(device)
 
-    return net, device
+    return net
+
+
+
 
 if __name__ == "__main__":
     main()
+
