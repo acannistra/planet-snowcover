1281533ee14671a7f009fa26350d6947f5ef4937
Tony Cannistra (EC2) <tony.cannistra@gmail.com>
2019-08-05 20:09:34 +0000

    working on start script

--- sagemaker/start.sh
+++ sagemaker/start.sh
@@ -5,11 +5,14 @@
 
 source activate pytorch_p36
 
-if [ "$1" != "" ]; then
+if [ "$1" == "train" ]; then
     echo "Initiating training with config $2."
-    wget $2 -O "model-config.toml"
+    ls /opt/ml/input/data/config/*.toml
     cd robosat_pink/
     ./rsp train --config /opt/ml/input/data/config/*.toml /opt/ml/model/
+elif [ "$1" == "lab" ]; then 
+    echo "Starting Jupyterlab...(port:8888)"
+    jupyter lab --allow-root --ip=0.0.0.0 
 fi
 
 
