18c8c3402c57ba29881d766387f26a0284ffce99
Tony Cannistra <tony.cannistra@gmail.com>
2019-08-20 12:47:52 -0700

    updates

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 
+
 # -- Project information -----------------------------------------------------
 
 project = 'CubeSat SCA'
@@ -27,6 +28,8 @@ author = 'Tony Cannistra'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+
+extensions = ['recommonmark']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
