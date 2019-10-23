77ff535633c927704fc24b8033e7689115f568bd
Tony Cannistra <tony.cannistra@gmail.com>
2019-10-23 14:15:20 -0700

    adding resources?

---
+++ deployment/outputs.tf
@@ -0,0 +1,15 @@
+output "vpc_id" {
+  value = "${aws_vpc.vpc.id}"
+}
+output "public_subnets" {
+  value = ["${aws_subnet.subnet_public.id}"]
+}
+output "public_route_table_ids" {
+  value = ["${aws_route_table.rtb_public.id}"]
+}
+output "public_instance_ip" {
+  value = ["${aws_instance.testInstance.public_ip}"]
+}
+output "SSH_HERE" {
+  value = "ssh -i ${var.private_key} ubuntu@${aws_instance.testInstance.public_ip}"
+}
