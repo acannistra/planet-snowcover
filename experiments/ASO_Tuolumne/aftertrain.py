91671169497f720cb260fcdd668d93d14f48c8ec
Tony Cannistra <tony.cannistra@gmail.com>
2020-02-10 14:45:58 -0800

    afertrain arguments

--- experiments/ASO_Tuolumne/aftertrain.py
+++ experiments/ASO_Tuolumne/aftertrain.py
@@ -12,7 +12,7 @@ from subprocess import Popen, PIPE
 @click.option("--aws_profile", "-p", "aws_profile", required=True)
 @click.option("--aws_region", "-r", required=True)
 @click.option("--output", '-o', "output_dir", required=False)
-@click.option("--checkpoint", "checkpoint_name", help="which checkpoint to extract?", default="checkpoint-00050-of-00050.pth")
+@click.option("--checkpoint", "checkpoint_name", help="which checkpoint to extract? [default: checkpoint-00050-of-00050.pth]", default="checkpoint-00050-of-00050.pth")
 def process_training_output(result_path, aws_profile, aws_region, output_dir, checkpoint_name):
     sess = boto3.Session(profile_name=aws_profile, region_name=aws_region)
     fs = s3fs.S3FileSystem(session = sess)
