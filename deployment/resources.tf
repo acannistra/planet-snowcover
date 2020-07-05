1241b5722948cbd097d11ed946b43312db3ee4f5
Tony Cannistra <tony.cannistra@gmail.com>
2020-07-05 13:19:31 -0700

    terraform syntax upgrade

--- deployment/resources.tf
+++ deployment/resources.tf
@@ -1,80 +1,80 @@
 #providers
 provider "aws" {
-  region                  = "${var.region}"
-  shared_credentials_file = "~/.aws/creds"
-  profile                 = "${var.aws_profile}"
+  region                  = var.region
+  shared_credentials_file = "~/.aws/credentials"
+  profile                 = var.aws_profile
 }
 
 #resources
 
 ## ECR
 resource "aws_ecr_repository" "ps_ecr" {
-  name = "ps-images"
+  name                 = "ps-images"
   image_tag_mutability = "MUTABLE"
 }
 
 ## EC2
 resource "aws_vpc" "vpc" {
-  cidr_block = "${var.cidr_vpc}"
+  cidr_block           = var.cidr_vpc
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Environment = "${var.environment_tag}"
+    Environment = var.environment_tag
   }
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
   tags = {
-    Environment = "${var.environment_tag}"
+    Environment = var.environment_tag
   }
 }
 
 resource "aws_subnet" "subnet_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
-  cidr_block = "${var.cidr_subnet}"
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.cidr_subnet
   map_public_ip_on_launch = "true"
-  availability_zone = "${var.availability_zone}"
+  availability_zone       = var.availability_zone
   tags = {
-    Environment = "${var.environment_tag}"
+    Environment = var.environment_tag
   }
 }
 
 resource "aws_route_table" "rtb_public" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
   route {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = "${aws_internet_gateway.igw.id}"
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 
   tags = {
-    Environment = "${var.environment_tag}"
+    Environment = var.environment_tag
   }
 }
 
 resource "aws_route_table_association" "rta_subnet_public" {
-  subnet_id      = "${aws_subnet.subnet_public.id}"
-  route_table_id = "${aws_route_table.rtb_public.id}"
+  subnet_id      = aws_subnet.subnet_public.id
+  route_table_id = aws_route_table.rtb_public.id
 }
 
 resource "aws_security_group" "sg_22_80" {
-  name = "sg_22"
-  vpc_id = "${aws_vpc.vpc.id}"
+  name   = "sg_22"
+  vpc_id = aws_vpc.vpc.id
 
   # SSH access from the VPC
   ingress {
-      from_port   = 22
-      to_port     = 22
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-      from_port   = 80
-      to_port     = 80
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -85,14 +85,13 @@ resource "aws_security_group" "sg_22_80" {
   }
 
   tags = {
-    Environment = "${var.environment_tag}"
+    Environment = var.environment_tag
   }
 }
 
 resource "aws_key_pair" "ec2key" {
-
-  key_name = "publicKey"
-  public_key = "${file(var.public_key)}"
+  key_name   = "publicKey"
+  public_key = file(var.public_key)
 }
 
 ## Create acess role for EC2 instance.
@@ -114,52 +113,52 @@ resource "aws_iam_role" "EC2_access_role" {
   ]
 }
 EOF
+
 }
 
 # Attach policy to above role.
 resource "aws_iam_role_policy_attachment" "ec2_enable" {
-  role = "${aws_iam_role.EC2_access_role.name}"
-  policy_arn = "${var.ec2_default_policy_arn}"
+  role       = aws_iam_role.EC2_access_role.name
+  policy_arn = var.ec2_default_policy_arn
 }
 
 # Create instance profile for EC2 instance w/ above  role
 resource "aws_iam_instance_profile" "ec2_instance_profile" {
   name = "main_ec2_profile"
-  role = "${aws_iam_role.EC2_access_role.name}"
+  role = aws_iam_role.EC2_access_role.name
 }
 
 # create instance
 resource "aws_instance" "mainDevInstance" {
-  ami           = "${var.instance_ami}"
-  instance_type = "${var.instance_type}"
-  subnet_id = "${aws_subnet.subnet_public.id}"
-  vpc_security_group_ids = ["${aws_security_group.sg_22_80.id}"]
-  key_name = "${aws_key_pair.ec2key.key_name}"
-  iam_instance_profile = "${aws_iam_instance_profile.ec2_instance_profile.name}"
+  ami                    = var.instance_ami
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.subnet_public.id
+  vpc_security_group_ids = [aws_security_group.sg_22_80.id]
+  key_name               = aws_key_pair.ec2key.key_name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_instance_profile.name
 
   tags = {
-		Environment = "${var.environment_tag}"
-	}
+    Environment = var.environment_tag
+  }
 
-	provisioner "file" {
+  provisioner "file" {
     source      = "~/.aws/"
     destination = "/home/ubuntu/"
-
   }
 
   provisioner "remote-exec" {
     inline = [
       "sudo docker pull ${var.DOCKERHUB_IMAGE}",
-			"sudo docker tag ${var.DOCKERHUB_IMAGE} ${aws_ecr_repository.ps_ecr.repository_url}:latest",
-      "sudo docker push ${aws_ecr_repository.ps_ecr.repository_url}:latest"
+      "sudo docker tag ${var.DOCKERHUB_IMAGE} ${aws_ecr_repository.ps_ecr.repository_url}:latest",
+      "sudo docker push ${aws_ecr_repository.ps_ecr.repository_url}:latest",
     ]
   }
 
   connection {
-    type     = "ssh"
-    user     = "ubuntu"
-		host     = "${self.public_dns}"
-    private_key = "${file(var.private_key)}"
+    type        = "ssh"
+    user        = "ubuntu"
+    host        = self.public_dns
+    private_key = file(var.private_key)
   }
 }
 
@@ -180,6 +179,7 @@ resource "aws_iam_role" "sagemaker_role" {
     ]
   }
 EOF
+
 }
 
 resource "aws_iam_policy" "s3-access-policy" {
@@ -203,14 +203,16 @@ resource "aws_iam_policy" "s3-access-policy" {
   	}]
 }
 EOF
+
 }
 
 resource "aws_iam_role_policy_attachment" "attach-sage-fullaccess" {
-  role       = "${aws_iam_role.sagemaker_role.name}"
+  role       = aws_iam_role.sagemaker_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "attach-sage-s3access" {
-  role       = "${aws_iam_role.sagemaker_role.name}"
-  policy_arn = "${aws_iam_policy.s3-access-policy.arn}"
+  role       = aws_iam_role.sagemaker_role.name
+  policy_arn = aws_iam_policy.s3-access-policy.arn
 }
+
--- deployment/variables.tf
+++ deployment/variables.tf
@@ -1,48 +1,59 @@
 # @@@@@@@@@@ Important Variables
 variable "aws_profile" {
-  default = "esip"
+  default = "snowmap"
 }
+
 variable "public_key" {
   description = "Public key path"
-  default = "~/.ssh/esip-cannistra.pub"
+  default     = "~/.ssh/snowmap.pub"
 }
+
 variable "private_key" {
-  default = "~/.ssh/esip-cannistra.pem"
+  default = "~/.ssh/snowmap.pem"
 }
 
 # You can leave these alone.
 variable "region" {
   default = "us-west-2"
 }
+
 variable "availability_zone" {
   description = "availability zone to create subnet"
-  default = "us-west-2a"
+  default     = "us-west-2a"
 }
+
 variable "instance_type" {
   description = "type for aws EC2 instance"
-  default = "t3.medium"
+  default     = "t3.medium"
 }
+
 variable "cidr_vpc" {
   description = "CIDR block for the VPC"
-  default = "10.1.0.0/16"
+  default     = "10.1.0.0/16"
 }
+
 variable "cidr_subnet" {
   description = "CIDR block for the subnet"
-  default = "10.1.0.0/24"
+  default     = "10.1.0.0/24"
 }
+
 variable "instance_ami" {
   description = "AMI for aws EC2 instance"
-  default = "ami-001083bce630cc561"
+  default     = "ami-001083bce630cc561"
 }
-variable "ec2_default_policy_arn"{
+
+variable "ec2_default_policy_arn" {
   description = "default policy to attach to EC2 role (via ARN)."
-  default = "arn:aws:iam::aws:policy/PowerUserAccess"
+  default     = "arn:aws:iam::aws:policy/PowerUserAccess"
 }
+
 variable "environment_tag" {
   description = "Environment tag"
-  default = "Production"
+  default     = "Production"
 }
+
 variable "DOCKERHUB_IMAGE" {
   description = "Dockerhub location of main toolset image for dev + sage."
-  default = "tonycannistra/planet-snowcover:latest"
+  default     = "tonycannistra/planet-snowcover:latest"
 }
+
