658fa403badf18f9de1185841f51245de1db9ef3
Tony Cannistra <tony.cannistra@gmail.com>
2020-08-16 15:50:11 +0000

    remove credential dependency in dockerfile

--- environment/create-aws-creds.sh
+++
@@ -1,6 +0,0 @@
-KEY=$AWS_ACCESS_KEY_ID
-SECRET=$AWS_SECRET_ACCESS_KEY
-PROFILE="esip"
-
-printf "[profile $PROFILE]\nregion = us-west-2 "> ./environment/aws_config
-printf "[$PROFILE]\naws_access_key_id=$KEY\naws_secret_access_key=$SECRET" > ./environment/aws_creds
